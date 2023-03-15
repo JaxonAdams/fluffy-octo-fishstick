30e5a4673a630b3267dcf5a4cc869ef77a2c6a6c
Jaxon Adams <jadamsresume1@gmail.com>
2023-03-14 20:08:57 -0600

    created header and hero components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
+import Home from './pages/Home';
+
 function App() {
   return (
     <div className='App'>
-      <h1 className='title'>Hello World</h1>
+      <Home />
     </div>
   );
-}
+};
 
 export default App;
