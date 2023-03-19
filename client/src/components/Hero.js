25754dd4bac842d68dcb72312ddbda5fc08ba8e8
Jaxon Adams <jadamsresume1@gmail.com>
2023-03-18 19:15:43 -0600

    added profile to hero

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import profile from '../assets/jaxon.png';
+
 const Hero = () => {
   return (
     <div className='hero'>
@@ -7,7 +9,10 @@ const Hero = () => {
         <h3 className='subtitle'>Hello, my name is</h3>
         <h1 className='title hero-title'><span className='highlight-txt'>Jaxon</span> Adams</h1>
       </div>
-      <div className='hero-container container-right'></div>
+      <div className='hero-container container-right'>
+        <img className='profile-img' src={ profile } alt='Jaxon Adams' />
+        <div className='profile-bg' />
+      </div>
     </div>
   );
 };
