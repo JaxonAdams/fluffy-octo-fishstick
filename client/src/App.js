4eec1cc7f15b06ed932a6820d8ff33be06cc68b6
Jaxon Adams <jadamsresume1@gmail.com>
2023-05-16 15:23:56 -0600

    set up react router for multiple pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home/Home';
+import AboutMe from './pages/AboutMe/AboutMe';
 
 function App() {
   return (
     <div className='App'>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/aboutme' element={<AboutMe />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
